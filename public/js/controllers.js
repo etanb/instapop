9d86e389c3e15107c83db2cfc7b27795f176bcf9
etanb <etan.berkowitz@gmail.com>
2014-10-31 15:51:19 -0700

    cleaned up code a bit

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,17 +1,8 @@
 var instaPopApp = angular.module('instaPop', []);
 
 instaPopApp.controller('PopularListController', function($scope, $http){
-  $http.jsonp('https://api.instagram.com/v1/media/popular', {
-    params: {
-      client_id: '4d5374af01d74b18a3ff5372cdb42567',
-      count: 10,
-      callback: 'JSON_CALLBACK'
-    }
-  }).success(function(data) {
-    $scope.posts = data.data
-  })
-  $scope.loadPosts = function () {
-    console.log("button is firing")
+
+  function popularMediaCall () {
     $http.jsonp('https://api.instagram.com/v1/media/popular', {
       params: {
         client_id: '4d5374af01d74b18a3ff5372cdb42567',
@@ -21,5 +12,11 @@ instaPopApp.controller('PopularListController', function($scope, $http){
     }).success(function(data) {
       $scope.posts = data.data
     })
+  }
+
+  popularMediaCall()
+  $scope.loadPosts = function () {
+    console.log("button is firing")
+    popularMediaCall()
   }
 })
